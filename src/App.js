1632c055321fa55e41870be7c785913fd0723b36
Isaque Barbosa <isaquebatista400@gmail.com>
2024-04-26 16:19:43 -0300

    Criando o Primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import Button from "./components/Button/";
 function App() {
   return (
     <div className="App">
-      Olá React
+      <h1>Olá React</h1>
+      <Button title="Entrar"/>
+      <Button title="Sair"/>
+      <Button title="Enviar"/>
+      <Button title="Bloquear"/>
+      <Button title="Excluir"/>
+      <Button title="Privar"/>
     </div>
-  );
+  )
 }
 
 export default App;
