e3f204d9f31ade4d0824c2ba313cfa0e35998325
Isaque Barbosa <isaquebatista400@gmail.com>
2024-04-30 16:00:10 -0300

    Trabalhando com a sintaxe JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const App = () => {
+  const arr = [1,2,3,4,5,6,7,8,9,10]
   return(
     <div className="App">
       <h1>Hello World</h1>
+      {arr.map((item) => (
+        <p>
+          {item} * 5 = {item*5}
+        </p>
+      ))}
     </div>
   )
 }
